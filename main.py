77e905b1022e93a17de29ede0d68a02f9951d668
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-03-30 16:22:41 -0400

    if valid address, check for demos nearby

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, redirect
 from datetime import datetime
+from sodapy import Socrata
 import twilio.twiml
 import os, requests, json, urllib
 import geocode
+
+soda_client = Socrata("data.detroitmi.gov", os.environ['SODA_TOKEN'], os.environ['SODA_USER'], os.environ['SODA_PASS'])
 
 app = Flask(__name__)
 
@@ -19,11 +22,20 @@ def initial():
     located = geocode.best_parcel_match(body)
     print(located)
 
-    # default messages for valid or invalid addresses
+    # if it's a valid address, check if it has demos nearby 
     if located:
-    	resp.message("List of properties nearby {} scheduled to be demolished in the next few days. Subscribe to get future alerts for this address.".format(located['address']))
+        lat = located['coords'][0]
+        lng = located['coords'][1]
+        demos = soda_client.get("q48r-nkgw", where="within_circle(location, {}, {}, 155)".format(lat, lng))
+        print(demos)
+
+        if len(demos) > 0:
+            resp.message("Demos scheduled near {}: {}.".format(located['address'], demos))
+        else:
+            resp.message("No demos scheduled near {} in the next 5 days. Text 'Y' to subscribe to future demo alerts near this address.".format(located['address']))
+    # default message for a bad address
     else:
-    	resp.message("To receive notices about demolitions happening nearby, please text us a street address (eg '123 Woodward').")
+        resp.message("To receive notices about demolitions happening nearby, please text us a street address (eg '123 Woodward').")
 
     return str(resp)
 
