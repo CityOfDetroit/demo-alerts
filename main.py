1c3cc1a51b97e03fe45768d664608b35934e0798
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-04-05 20:10:27 -0400

    add a class to build the demo text msg, still figuring out what a class is

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import twilio.twiml
 import os, requests, json, urllib
 from geocoder import Geocoder
 import contact
+import message
 
 soda_client = Socrata("data.detroitmi.gov", os.environ['SODA_TOKEN'], os.environ['SODA_USER'], os.environ['SODA_PASS'])
 
@@ -17,6 +18,8 @@ def initial():
     """Respond to incoming calls with a simple text message."""
 
     resp = twilio.twiml.Response()
+    
+    # get sender phone number, check if they are a current subscriber
     incoming_number = request.values.get('From')[2:]
     if incoming_number in users.keys():
         caller = users[incoming_number]
@@ -24,15 +27,19 @@ def initial():
     else:
         caller = contact.Contact(incoming_number)
     print(caller)
+
     # get body of incoming SMS
     body = request.values.get('Body')
+
+    # check if the body is 'add' or 'end'
     if body.upper() == 'ADD' and caller.last_requested_address:
         caller.watch(caller.last_requested_address)
         resp.message("You've subscribed to demolition alerts near {}. Text 'END' to unsubscribe from your alerts.".format(caller.last_requested_address))
+
         # remove from users so we grab a 'fresh' copy of the user with sheet rows
         del users[incoming_number]
         return str(resp)
-    if body.upper() == 'END':
+    elif body.upper() == 'END':
         # handle unsub here
         pass
     else:
@@ -42,27 +49,14 @@ def initial():
 
         # if it's a valid address, check if it has demos nearby
         if located:
-            lat = located['location']['y']
-            lng = located['location']['x']
-            # print(lat, lng)
+            demo_msg = message.Message(located)
+            print(demo_msg)
 
-            # query against socrata dataset, returns an array
-            # eventually, add time query here too??
-            demos = soda_client.get("8wnn-qcxj", where="within_circle(location, {}, {}, 155)".format(lat, lng))
-            print(demos)
-
-            # if demos nearby, make a list of addresses and dates to add to the message
-            if len(demos) > 0:
-                list_demos = []
-                for d in demos:
-                    formatted_demo = "{} on {}".format(d['address'], datetime.fromtimestamp(int(d['demo_date'])).strftime('%m-%d-%Y'))
-                    list_demos.append(formatted_demo)
-
-                resp.message("{} demos scheduled near {} in the next 5 days: \n{}. \nDates subject to change. Text 'ADD' to subscribe to future demo alerts near this address.".format(len(demos), located['address'], (";\n").join(list_demos)))
+            # todo: figure out how to deal with this based on message returned (1+ or 0 demos nearby soon)
+            if demo_msg:
                 caller.last_requested_address = located['address'][:-7]
                 users[incoming_number] = caller
             else:
-                resp.message("No demos scheduled near {} in the next 5 days. Text 'ADD' to subscribe to future demo alerts near this address.".format(located['address']))
                 caller.last_requested_address = located['address'][:-7]
                 users[incoming_number] = caller
 
---
+++ message.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from sodapy import Socrata
+
+soda_client = Socrata("data.detroitmi.gov", os.environ['SODA_TOKEN'], os.environ['SODA_USER'], os.environ['SODA_PASS'])
+
+class Message(object):
+    def __init__(self, locatedAddress):
+        self.addr = locatedAddress
+        self.hasDemos = False
+
+    def make_demo_msg(self, addr):
+        """Find upcoming demolitions near an already geocoded address and list them in a text message"""
+        lat = addr['location']['y']
+        lng = addr['location']['x']
+
+        # query Socrata dataset for demos within 500ft (155m) and n days (todo)
+        demos = soda_client.get("8wnn-qcxj", where="within_circle(location, {}, {}, 155)".format(lat, lng))
+        print(demos)
+
+        # build the text
+        if len(demos) > 0:
+            self.hasDemos = True
+            list_demos = []
+            
+            for d in demos:
+                formatted_demo = "{} on {}".format(d['address'], datetime.fromtimestamp(int(d['demo_date'])).strftime('%m-%d-%Y'))
+                list_demos.append(formatted_demo)
+
+            return resp.message("{} demos scheduled near {} in the next 5 days: \n{}. \nDates subject to change. Text 'ADD' to subscribe to future demo alerts near this address.".format(len(demos), addr['address'], (";\n").join(list_demos))) 
+        else:
+            return resp.message("No demos scheduled near {} in the next 5 days. Text 'ADD' to subscribe to future demo alerts near this address.".format(addr['address']))
